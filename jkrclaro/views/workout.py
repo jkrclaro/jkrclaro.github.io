e0faaa3d746f07a38730b6e7f96ae10e7d42217a
John Claro <jkrclaro@outlook.com>
2018-06-28 22:13:41 +0100

    First

--- jkrclaro/views/workout.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, render_template
-
-workout = Blueprint('workout', __name__, url_prefix='/workout/')
-
-
-@workout.route('/')
-def index():
-    return render_template('workout.html')
