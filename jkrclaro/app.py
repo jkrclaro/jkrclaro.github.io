cc3bac8922fc46fa6970864b16cc86338edef945
John Claro <jkrclaro@outlook.com>
2018-06-28 21:43:14 +0100

    Run

--- jkrclaro/app.py
+++ jkrclaro/app.py
@@ -1,6 +1,12 @@
 from flask import Flask
-app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+
+def create_app():
+    app = Flask(__name__)
+
+    from .views.workout import workout
+    from .views.home import home
+    app.register_blueprint(workout)
+    app.register_blueprint(home)
+
+    return app
