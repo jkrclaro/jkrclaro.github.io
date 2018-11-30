bcb4fb85009acf2c92f41bbf1816766b10d4eedb
John Claro <jkrclaro@outlook.com>
2018-11-30 15:00:36 +0000

    Hello John

---
+++ precache-manifest.04be5aeacf6a650adee4e17ad1226a02.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "460e03e988d23ebc1cfd",
+    "url": "/static/js/main.460e03e9.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "460e03e988d23ebc1cfd",
+    "url": "/static/css/main.804084af.chunk.css"
+  },
+  {
+    "revision": "03d6837c65df273cbb322e49f4da7f7c",
+    "url": "/index.html"
+  }
+];
