5d2e2e594471fd7ca441775809bc4e48c284a416
John Claro <jkrclaro@outlook.com>
2018-12-06 18:33:00 +0000

    Update with HashRouter

--- jkrclaro/src/App.js
+++ jkrclaro/src/App.js
@@ -1,20 +1,25 @@
 import React, { Component } from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 import Header from './components/Header';
 import Body from './components/Body';
 
 class App extends Component {
-  render() {
-    return (
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
-        <div>
-          <Header/>
-          <Body/>
-        </div>
-      </BrowserRouter>
-    );
-  }
+
+    render() {
+        return(
+            <HashRouter>
+                <div className='main-body jkrclaro-bg'>
+                    <div className='sub-body'>
+                        <Header />
+                        <div className='container'>
+                            <Body />
+                        </div>
+                    </div>
+                </div>
+            </HashRouter>
+        )
+    }
 }
 
 export default App;
--- jkrclaro/src/components/Body.jsx
+++ jkrclaro/src/components/Body.jsx
@@ -1,23 +1,17 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 
 import Home from './Home';
-import Library from './Library';
-import Blog from './Blog';
 import Portfolio from './Portfolio';
-import ErrorPage from './ErrorPage';
 
 class Body extends React.Component {
 
     render() {
         return (
-            <Switch>
-                <Route path='/' component={Home} exact />
-                <Route path='/blog' component={Blog} />
+            <div>
+                <Route exact path='/' component={Home} />
                 <Route path='/portfolio' component={Portfolio} />
-                <Route path='/library' component={Library} />
-                <Route component={ErrorPage} />
-            </Switch>
+            </div>
         )
     }
 }
