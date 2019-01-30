07b3c9364150dd14bdf1cee217e62955c8272438
John cClaro <jkrclaro@gmail.com>
2019-01-30 01:08:21 +0000

    Deploy

--- jkrclaro/src/App.js
+++ jkrclaro/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
+import Header from './components/Header';
 import Body from './components/Body';
 
 class App extends Component {
@@ -10,6 +11,9 @@ class App extends Component {
             <BrowserRouter basename={process.env.PUBLIC_URL}>
                 <div className='main-body jkrclaro-bg'>
                     <div className='sub-body'>
+                        <div className='App-header'>
+                            <Header />
+                        </div>
                         <div className='container'>
                             <Body />
                         </div>
