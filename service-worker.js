15a1a2a66bb9d25d8e9d1e219b795554aacd9459
John cClaro <jkrclaro@gmail.com>
2019-01-30 01:03:31 +0000

    Deploy: me

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ae4770c4bc06fc574a69871f8c5f121b.js"
+  "/precache-manifest.b813c33c506a774267054e801243e553.js"
 );
 
 workbox.clientsClaim();
