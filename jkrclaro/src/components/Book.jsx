b8029fdddc918ef6bb4ecf2eba6cd991a20eb949
John cClaro <jkrclaro@gmail.com>
2019-02-10 13:39:58 +0000

    Add To Sell Is Human: The Surprising Truth About Moving Others

--- jkrclaro/src/components/Book.jsx
+++ jkrclaro/src/components/Book.jsx
@@ -7,6 +7,7 @@ class Book extends React.Component {
         return (
             <div className='col-lg-12'>
                 <ul>
+                    <li><a href='https://www.amazon.com/Sell-Human-Surprising-Moving-Others/dp/1594631905'>To Sell Is Human: The Surprising Truth About Moving Others</a></li>
                     <li><a href='https://www.amazon.com/Algorithms-Live-Computer-Science-Decisions/dp/1627790365'>Algorithms to Live By: The Computer Science of Human Decisions</a></li>
                     <li><a href='https://www.amazon.com/Zero-One-Notes-Startups-Future/dp/0804139296'>Zero to One: Notes on Startups, or How to Build the Future</a></li>
                     <li><a href='https://www.amazon.com/Chaos-Monkeys-Obscene-Fortune-Failure-ebook/dp/B019MMUAAQ'>Chaos Monkeys: Obscene Fortune and Random Failure in Silicon Valley</a></li>
