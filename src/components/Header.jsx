e816da9d18d54d7f31fbc275c83af425b2411a31
John cClaro <jkrclaro@gmail.com>
2019-05-29 05:28:53 +0100

    Add Header.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+class Header extends React.Component {
+
+    render() {
+        return (
+            <div className='container mt-3'>
+                <ul className="nav justify-content">
+                    <li className="nav-item"><Link className='nav-link' to='/'>About</Link></li>
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default Header;
