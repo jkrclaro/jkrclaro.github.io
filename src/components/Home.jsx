f64de0e8d29ff22223052cdf563cd2bfda99dffe
John cClaro <jkrclaro@gmail.com>
2019-06-14 06:45:38 +0100

    Channelry

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -21,7 +21,7 @@ class Home extends React.Component {
                         <p>I graduated with a BSc Applied Computing in Waterford Institute of Technology from 2012-2016.</p>
                         <p>I live in Dublin, Ireland.</p>
                         <p>I worked for DoneDeal.ie which is Ireland's largest classified ads website from 2016-2018 as a Data Engineer.</p>
-                        <p>I'm currently working on <a href='https://netprechaun.com'>Netprechaun</a>, a simple marketplace management software.</p>
+                        <p>I'm currently working on <a href='https://channelry.com'>Channelry</a>.</p>
                         <p>If you want to contact me, you can reach me via one of the following communication channels:</p>
                         <ul>
                             <li>via <a href='mailto:jkrclaro@gmail.com' style={underlineStyle}>email</a></li>
