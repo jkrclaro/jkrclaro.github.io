744942dc2475119f65544d5dc93fd2a9f7ea4596
John cClaro <jkrclaro@gmail.com>
2019-06-16 14:41:26 +0100

    Update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -19,12 +19,12 @@ class Home extends React.Component {
                             software engineering and tons of other stuff. 
                         </p>
                         <p>I was born in Manila, Philippines but raised in Waterford, Ireland since I was 10 years old.</p>
-                        <p>I graduated with a BSc Applied Computing in Waterford Institute of Technology from 2012-2016 and did work placement for DoneDeal.ie which is Ireland's biggest classifieds website.</p>
-                        <p>After graduating, I moved to Dublin to work at DistilledSCH (owns DoneDeal.ie, Adverts.ie and Daft.ie) which is Ireland's leading online media company from 2016-2018 as a Data Engineer.</p>
+                        <p>I graduated with a BSc Applied Computing in Waterford Institute of Technology from 2012-2016 and did work placement at DoneDeal.ie which is Ireland's biggest classifieds website.</p>
+                        <p>After graduating, I moved to Dublin to work at DistilledSCH which is Ireland's leader in online marketplaces from 2016-2018 as a Data Engineer.</p>
                         <p>I left DistilledSCH to work on an idea that helps sellers, <a href='https://channelry.com'>Channelry</a>.</p>
                         <p>If you want to contact me, you can reach me via one of the following communication channels:</p>
                         <ul>
-                            <li>via <a href='mailto:john@channelry.com' style={underlineStyle}>email</a></li>
+                            <li>via <a href='mailto:john@channelry.com' style={underlineStyle}>john@channelry.com</a></li>
                             <li>send me a message on <a href='https://www.github.com/jkrclaro' target="_blank" rel="noopener noreferrer" style={underlineStyle}>github</a></li>
                             <li>reach out to me on <a href='https://www.twitter.com/jkrclaro' target="_blank" rel="noopener noreferrer" style={underlineStyle}>twitter</a></li>
                             <li>connect with me on <a href='https://www.linkedin.com/in/jkrclaro' target="_blank" rel="noopener noreferrer" style={underlineStyle}>linkedin</a></li>
