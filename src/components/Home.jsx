475ccf936df5f9a53ff130c3fef98e606294c8de
John cClaro <jkrclaro@gmail.com>
2019-12-06 03:07:36 +0000

    Add full stop

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -16,7 +16,7 @@ class Home extends React.Component {
                         <p>My name is John Claro, I'm interested in many aspects of software engineering.</p>
                         <p>I was born in Antipolo, Philippines then moved to Waterford, Ireland when I was 9 years old.</p>
                         <p>I graduated with Applied Computing in Waterford Institute of Technology.</p>
-                        <p>After graduating, I got a job as a data engineer for Ireland's largest online marketplace</p>
+                        <p>After graduating, I got a job as a data engineer for Ireland's largest online marketplace.</p>
                         <p>If you want to contact me, you can reach me via one of the following communication channels:</p>
                         <ul>
                             <li>via <a href='mailto:jkrclaro@gmail.com' style={underlineStyle}>email</a></li>
