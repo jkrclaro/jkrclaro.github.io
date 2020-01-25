ab6216f1f516efa57711c308c9a41d004dde6a6e
John cClaro <jkrclaro@gmail.com>
2020-01-25 21:31:28 +0000

    Update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,9 +5,8 @@ class Navbar extends React.Component {
 
     render() {
         return (
-            <ul className='nav mb-3'>
+            <ul className='nav mt-3 mb-3'>
                 <li className='nav-item nav-link'><Link to='/'>About</Link></li>
-                {/* <li className='nav-item nav-link'><Link to='/essays'>Essays</Link></li> */}
                 <li className='nav-item nav-link'><Link to='/books'>Books</Link></li>
             </ul>
         )
