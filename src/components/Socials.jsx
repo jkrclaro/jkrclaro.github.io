7e47111412a8e5a8e93082caa26ace9f2f2f7d7f
John cClaro <jkrclaro@gmail.com>
2020-01-25 21:14:25 +0000

    Update

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -5,7 +5,7 @@ class Socials extends Component {
 
     render() {
         return (
-            <div className='text-center' id='contacts'>
+            <div className='text-sm-center' id='contacts'>
                 <a href='mailto:jkrclaro@gmail.com'><i className='far fa-envelope mr-5'></i></a>
                 <a href='https://www.github.com/jkrclaro' target="_blank" rel="noopener noreferrer"><i className='fab fa-github mr-5'></i></a>
                 <a href='https://www.linkedin.com/in/jkrclaro' target="_blank" rel="noopener noreferrer"><i className='fab fa-linkedin-in mr-5'></i></a>
