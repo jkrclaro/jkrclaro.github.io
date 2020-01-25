5c6874c9ccd5aa590801ec5d61e6ccb975de2a6e
John cClaro <jkrclaro@gmail.com>
2020-01-25 21:11:23 +0000

    Update

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
     render() {
         return(
             <BrowserRouter>
-                <div className='container'>
+                <div className='container mb-3'>
                     <Navbar />
                     <Switch>
                         <Route exact path={process.env.PUBLIC_URL + '/'} component={Home} />
