a4f5878a216b6cc02294d79194eb78660b0a786e
John cClaro <jkrclaro@gmail.com>
2020-01-25 22:52:46 +0000

    Update

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -9,10 +9,10 @@ class Info extends Component {
             <div className='row mb-3 mt-4'>
                 <div className='col-sm-7 mb-3'>
                     <div className='row'>
-                        <div className='col-sm-2 col-3 my-auto'>
+                        <div className='col-md-2 col-sm-3 col-3 my-auto'>
                             <img alt='me.png' src='/me.png' height='50' width='50' style={{ borderRadius: '50%'}}></img>
                         </div>
-                        <div className='col-sm-10 col-9 my-auto'>
+                        <div className='col-md-10 col-sm-9 col-9 my-auto'>
                             <div>John Claro</div>
                             <small className='text-muted'>
                                 {essay.publishedAt}
