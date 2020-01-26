78610141f65be2c8bc37225a176ac57f5796fa2e
John cClaro <jkrclaro@gmail.com>
2020-01-26 13:05:42 +0000

    Update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,13 +9,13 @@ class Home extends React.Component {
                 <div className='col-lg-5'>
                     <img className='mb-3' alt='me.png' src='/me.png' height='75' width='75' style={{ borderRadius: '50%'}}></img>
                     <p>I live in Dublin and I'm a software engineer primarily interested in web development and cloud computing.</p>
-                    <p>I was born in the Philippines but grew up in Ireland and studied at WIT.</p>
-                    <p className='mb-3'>You can find me or contact me at:</p>
+                    <p>I was born in the Philippines but grew up in Ireland and previously studied applied computing at WIT.</p>
+                    <p className='mb-3'>You can contact me by:</p>
                     <ul id='home'>
-                        <li className='mb-1'>via <a href='mailto:jkrclaro@gmail.com'>email</a></li>
+                        <li className='mb-1'>sending me an <a href='mailto:jkrclaro@gmail.com'>email</a></li>
                         <li className='mb-1'>connect with me on <a href='https://www.linkedin.com/in/jkrclaro' target="_blank" rel="noopener noreferrer">linkedin</a></li>
                         <li className='mb-1'>reach out to me on <a href='https://www.twitter.com/jkrclaro' target="_blank" rel="noopener noreferrer">twitter</a></li>
-                        <li>see my projects on <a href='https://www.github.com/jkrclaro' target="_blank" rel="noopener noreferrer">github</a></li>
+                        <li>or see my projects on <a href='https://www.github.com/jkrclaro' target="_blank" rel="noopener noreferrer">github</a></li>
                     </ul>
                 </div>
             </div>
