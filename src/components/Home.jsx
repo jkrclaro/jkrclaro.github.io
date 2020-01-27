fc87fb1588ae54b5e0cc77d25a0b748a6e5d9d74
John cClaro <jkrclaro@gmail.com>
2020-01-27 09:33:47 +0000

    Update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,12 +11,12 @@ class Home extends React.Component {
                     <p>I live in Dublin and I'm a software developer primarily interested in web development and cloud computing.</p>
                     <p>I was born in the Philippines but grew up in Ireland and previously studied applied computing at WIT.</p>
                     <p className='mb-3'>If you want to get in touch, here are my links:</p>
-                    <ul id='home'>
-                        <li className='mb-1'>send me an <a href='mailto:jkrclaro@gmail.com'>email</a></li>
-                        <li className='mb-1'>connect with me on <a href='https://www.linkedin.com/in/jkrclaro' target="_blank" rel="noopener noreferrer">linkedin</a></li>
-                        <li className='mb-1'>reach out to me on <a href='https://www.twitter.com/jkrclaro' target="_blank" rel="noopener noreferrer">twitter</a></li>
-                        <li>see my projects on <a href='https://www.github.com/jkrclaro' target="_blank" rel="noopener noreferrer">github</a></li>
-                    </ul>
+                    <div id='links'>
+                        <div><span className='bulletpoint mr-3'>-</span>send me an <a href='mailto:jkrclaro@gmail.com'>email</a></div>
+                        <div><span className='bulletpoint mr-3'>-</span>connect with me on <a href='https://www.linkedin.com/in/jkrclaro' target="_blank" rel="noopener noreferrer">linkedin</a></div>
+                        <div><span className='bulletpoint mr-3'>-</span>reach out to me on <a href='https://www.twitter.com/jkrclaro' target="_blank" rel="noopener noreferrer">twitter</a></div>
+                        <div><span className='bulletpoint mr-3'>-</span>see my projects on <a href='https://www.github.com/jkrclaro' target="_blank" rel="noopener noreferrer">github</a></div>
+                     </div>
                 </div>
             </div>
         )
