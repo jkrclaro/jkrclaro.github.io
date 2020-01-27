b36146a9d467efe6d11cce4f6953b95cf5b7d6e4
John cClaro <jkrclaro@gmail.com>
2020-01-27 09:49:40 +0000

    Update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,10 +12,10 @@ class Home extends React.Component {
                     <p>I was born in the Philippines but grew up in Ireland and previously studied applied computing at WIT.</p>
                     <p className='mb-3'>If you want to get in touch, below are my links:</p>
                     <div id='links'>
-                        <div><span className='bulletpoint mr-3'>-</span>send me an <a href='mailto:jkrclaro@gmail.com'>email</a></div>
-                        <div><span className='bulletpoint mr-3'>-</span>connect with me on <a href='https://www.linkedin.com/in/jkrclaro' target="_blank" rel="noopener noreferrer">linkedin</a></div>
-                        <div><span className='bulletpoint mr-3'>-</span>reach out to me on <a href='https://www.twitter.com/jkrclaro' target="_blank" rel="noopener noreferrer">twitter</a></div>
-                        <div><span className='bulletpoint mr-3'>-</span>see my projects on <a href='https://www.github.com/jkrclaro' target="_blank" rel="noopener noreferrer">github</a></div>
+                        <div><span className='bulletpoint mr-2'>-</span>send me an <a href='mailto:jkrclaro@gmail.com'>email</a></div>
+                        <div><span className='bulletpoint mr-2'>-</span>connect with me on <a href='https://www.linkedin.com/in/jkrclaro' target="_blank" rel="noopener noreferrer">linkedin</a></div>
+                        <div><span className='bulletpoint mr-2'>-</span>reach out to me on <a href='https://www.twitter.com/jkrclaro' target="_blank" rel="noopener noreferrer">twitter</a></div>
+                        <div><span className='bulletpoint mr-2'>-</span>see my projects on <a href='https://www.github.com/jkrclaro' target="_blank" rel="noopener noreferrer">github</a></div>
                      </div>
                 </div>
             </div>
