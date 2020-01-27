b6e25a4f64cda6b763804ae8f05fc3dea7988929
John cClaro <jkrclaro@gmail.com>
2020-01-27 22:21:00 +0000

    Update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,12 +10,13 @@ class Home extends React.Component {
                     <img className='mb-3' alt='me.png' src='/me.png' height='75' width='75' style={{ borderRadius: '50%'}}></img>
                     <p>I live in Dublin and I'm a software developer primarily interested in web development and cloud computing.</p>
                     <p>I was born in the Philippines, grew up in Ireland and previously studied applied computing at WIT.</p>
-                    <p className='mb-3'>Feel free to get in touch:</p>
+                    <p className='mb-3'>I can also be found on:</p>
                     <div id='links'>
                         <div><span className='bulletpoint mr-2'>-</span>send me an <a href='mailto:jkrclaro@gmail.com'>email</a></div>
-                        <div><span className='bulletpoint mr-2'>-</span>connect with me on <a href='https://www.linkedin.com/in/jkrclaro' target="_blank" rel="noopener noreferrer">linkedin</a></div>
-                        <div><span className='bulletpoint mr-2'>-</span>reach out to me on <a href='https://www.twitter.com/jkrclaro' target="_blank" rel="noopener noreferrer">twitter</a></div>
-                        <div><span className='bulletpoint mr-2'>-</span>see my projects on <a href='https://www.github.com/jkrclaro' target="_blank" rel="noopener noreferrer">github</a></div>
+                        <div><span className='bulletpoint mr-2'>-</span>connect with me on <a href='https://www.linkedin.com/in/jkrclaro' target='_blank' rel='noopener noreferrer'>linkedin</a></div>
+                        <div><span className='bulletpoint mr-2'>-</span>reach out to me on <a href='https://www.twitter.com/jkrclaro' target='_blank' rel='noopener noreferrer'>twitter</a></div>
+                        <div><span className='bulletpoint mr-2'>-</span>see my projects on <a href='https://www.github.com/jkrclaro' target='_blank' rel='noopener noreferrer'>github</a></div>
+                        <div><span className='bulletpoint mr-2'>-</span>recommended articles on <a href='https://getpocket.com/@jkrclaro' target='_blank' rel='noopener noreferrer'>pocket</a></div>
                      </div>
                 </div>
             </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,10 +6,14 @@ class Navbar extends React.Component {
     render() {
         const pathname = new URL(window.location.href).pathname;
         const articles = require('../articles.json')
+        let articleUrls = [];
+        for (const article of articles) {
+            articleUrls.push(article.url)
+        }
         return (
             <div className='nav mt-3 mb-3'>
                 <div className='mr-4'><a className={pathname === '/' ? 'link-active' : ''} href='/'>About</a></div>
-                <div className='mr-4'><a className={pathname === '/articles' || articles.includes(pathname) ? 'link-active' : ''} href='/articles'>Articles</a></div>
+                <div className='mr-4'><a className={pathname === '/articles' || articleUrls.includes(pathname) ? 'link-active' : ''} href='/articles'>Articles</a></div>
             </div>
         )
     }
