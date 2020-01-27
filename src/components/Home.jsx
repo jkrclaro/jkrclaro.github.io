3f69167323439f93aa8e240c9a7b374d03f1b24e
John cClaro <jkrclaro@gmail.com>
2020-01-27 18:44:39 +0000

    Update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,8 +9,8 @@ class Home extends React.Component {
                 <div className='col-lg-5'>
                     <img className='mb-3' alt='me.png' src='/me.png' height='75' width='75' style={{ borderRadius: '50%'}}></img>
                     <p>I live in Dublin and I'm a software developer primarily interested in web development and cloud computing.</p>
-                    <p>I was born in the Philippines but grew up in Ireland and previously studied applied computing at WIT.</p>
-                    <p className='mb-3'>If you want to get in touch, below are my links:</p>
+                    <p>I was born in the Philippines, grew up in Ireland and previously studied applied computing at WIT.</p>
+                    <p className='mb-3'>If you want to get in touch:</p>
                     <div id='links'>
                         <div><span className='bulletpoint mr-2'>-</span>send me an <a href='mailto:jkrclaro@gmail.com'>email</a></div>
                         <div><span className='bulletpoint mr-2'>-</span>connect with me on <a href='https://www.linkedin.com/in/jkrclaro' target="_blank" rel="noopener noreferrer">linkedin</a></div>
