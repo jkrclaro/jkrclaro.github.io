f6ef27035fe6f1bfdba9610e5c8f883ba05bbd5c
John cClaro <jkrclaro@gmail.com>
2020-01-28 18:41:33 +0000

    Update

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -4,7 +4,7 @@ import React, { Component } from 'react';
 class Article extends Component {
 
     componentDidMount() {
-        document.title = this.props.title;
+        document.title = `${this.props.title} - John Claro`;
     }
 
     render() {
