e09fae35d44bb028a306562c9498aaa77b1e145d
John cClaro <jkrclaro@gmail.com>
2020-01-29 12:35:30 +0000

    Update

--- src/App.js
+++ src/App.js
@@ -5,31 +5,29 @@ import Home from './components/Home';
 import NotFound from './components/NotFound';
 import Navbar from './components/Navbar';
 
-import Essay from './components/Essay';
-import Essays from './components/Essays';
-import BenjaminFranklin from './components/Essays/BenjaminFranklin';
-import Workspace from './components/Essays/Workspace';
+import Note from './components/Note';
+import Notes from './components/Notes';
+import Workspace from './components/Notes/Workspace';
 
 class App extends Component {
 
     state = {
-        essays: [
-            // {'url': '/benjamin-franklin', 'component': <Essay title='Benjamin Franklin: An American Life' publishedAt='Jan 25, 2019' readingTime='1m 9s' content={BenjaminFranklin} />},
-            {'url': '/workspace', 'component': <Essay title='Workspace' publishedAt='Jan 25, 2019' readingTime='1m 9s' content={Workspace} />}
+        notes: [
+            {'url': '/workspace', 'component': <Note title='Workspace' publishedAt='Jan 25, 2019' readingTime='1m 9s' content={Workspace} />}
         ]
     }
 
     render() {
-        const { essays } = this.state;
+        const { notes } = this.state;
         return(
             <BrowserRouter>
                 <div className='container mb-5'>
-                    <Navbar essays={essays} />
+                    <Navbar notes={notes} />
                     <Switch>
                         <Route exact path={process.env.PUBLIC_URL + '/'} component={Home} />
-                        <Route exact path={process.env.PUBLIC_URL + '/essays'} component={() => <Essays essays={essays} />} />
-                        {essays.map((article, index) =>
-                            <Route key={index} exact path={process.env.PUBLIC_URL + article.url} component={() => article.component} />
+                        <Route exact path={process.env.PUBLIC_URL + '/notes'} component={() => <Notes notes={notes} />} />
+                        {notes.map((note, index) =>
+                            <Route key={index} exact path={process.env.PUBLIC_URL + note.url} component={() => note.component} />
                         )}
                         <Route component={NotFound} />
                     </Switch>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,15 +5,15 @@ class Navbar extends React.Component {
 
     render() {
         const pathname = new URL(window.location.href).pathname;
-        const { essays } = this.props;
+        const { notes } = this.props;
         let articleUrls = [];
-        for (const article of essays) {
+        for (const article of notes) {
             articleUrls.push(article.url)
         }
         return (
             <div className='nav mt-3 mb-3'>
                 <div className='mr-4'><a className={pathname === '/' ? 'link-active' : ''} href='/'>About</a></div>
-                <div className='mr-4'><a className={pathname === '/essays' || articleUrls.includes(pathname) ? 'link-active' : ''} href='/essays'>Essays</a></div>
+                <div className='mr-4'><a className={pathname === '/notes' || articleUrls.includes(pathname) ? 'link-active' : ''} href='/notes'>Notes</a></div>
             </div>
         )
     }
