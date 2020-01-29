92538b4b654829dc48b86b0e98ddf8b044cca01b
John cClaro <jkrclaro@gmail.com>
2020-01-29 19:45:15 +0000

    Change to note

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,14 +6,14 @@ class Navbar extends React.Component {
     render() {
         const pathname = new URL(window.location.href).pathname;
         const { notes } = this.props;
-        let articleUrls = [];
-        for (const article of notes) {
-            articleUrls.push(article.url)
+        let noteUrls = [];
+        for (const note of notes) {
+            noteUrls.push(note.url)
         }
         return (
             <div className='nav mt-3 mb-3'>
                 <div className='mr-4'><a className={pathname === '/' ? 'link-active' : ''} href='/'>About</a></div>
-                <div className='mr-4'><a className={pathname === '/notes' || articleUrls.includes(pathname) ? 'link-active' : ''} href='/notes'>Notes</a></div>
+                <div className='mr-4'><a className={pathname === '/notes' || noteUrls.includes(pathname) ? 'link-active' : ''} href='/notes'>Notes</a></div>
             </div>
         )
     }
