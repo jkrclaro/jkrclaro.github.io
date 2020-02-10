b888f1e813eb447337ad866aba1acc6c1b23ae84
John cClaro <jkrclaro@gmail.com>
2020-02-10 02:06:57 +0000

    Hide essays nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,7 +15,7 @@ class Navbar extends React.Component {
         return (
             <div className='nav mt-3 mb-3'>
                 <div className='mr-4'><a className={pathname === '/' ? 'link-active' : ''} href='/'>About</a></div>
-                <div className='mr-4'><a className={pathname === '/essays' || essays.includes(pathname) ? 'link-active' : ''} href='/essays'>Essays</a></div>
+                {/* <div className='mr-4'><a className={pathname === '/essays' || essays.includes(pathname) ? 'link-active' : ''} href='/essays'>Essays</a></div> */}
             </div>
         )
     }
