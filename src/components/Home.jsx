1b2f6d27e1aee7e006a12a5fc74cbb0017dd01bd
John Claro <jkrclaro@gmail.com>
2020-05-11 03:27:13 +0100

    Update landing page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,17 +10,23 @@ class Home extends React.Component {
     render() {
         return (
             <div className='row'>
-                <div className='col-lg-5'>
+                <div className='col-lg-5 mb-3'>
                     <img className='mb-3' alt='me.png' src='/me.png' height='75' width='75' style={{ borderRadius: '50%'}}></img>
                     <p>I live in Dublin and I'm a software developer with a peculiar interest in data engineering and cloud computing.</p>
                     <p>I was born in the Philippines, grew up in Ireland and previously studied applied computing at WIT.</p>
-                    <p className='mb-3'>My links:</p>
+                    <p className='mb-3'>Links:</p>
                     <div id='links'>
                         <div><span className='bulletpoint mr-2'>-</span>send me an <a href='mailto:jkrclaro@gmail.com'>email</a></div>
                         <div><span className='bulletpoint mr-2'>-</span>connect with me on <a href='https://www.linkedin.com/in/johnclaro' target='_blank' rel='noopener noreferrer'>linkedin</a></div>
                         <div><span className='bulletpoint mr-2'>-</span>reach out to me on <a href='https://www.twitter.com/johnclaro_' target='_blank' rel='noopener noreferrer'>twitter</a></div>
-                        <div><span className='bulletpoint mr-2'>-</span>see my projects on <a href='https://www.github.com/johnclaro' target='_blank' rel='noopener noreferrer'>github</a></div>
                         <div><span className='bulletpoint mr-2'>-</span>recommended articles on <a href='https://getpocket.com/@johnclaro' target='_blank' rel='noopener noreferrer'>pocket</a></div>
+                     </div>
+                </div>
+                <div className='col-lg-7'>
+                    <p className='mb-3'>Projects:</p>
+                    <div id='links'>
+                        <div><span className='bulletpoint mr-2'>-</span>read my bug-ridden codes on <a href='https://www.github.com/johnclaro' target='_blank' rel='noopener noreferrer'>github</a></div>
+                        <div><span className='bulletpoint mr-2'>-</span><a href='https://chess.johnclaro.com'>chess</a></div>
                      </div>
                 </div>
             </div>
