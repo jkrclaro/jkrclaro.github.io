26ec40d85e7eba3b79e1278fe14ed22925d2838d
John Claro <jkrclaro@gmail.com>
2020-05-19 20:27:03 +0100

    Update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,6 +11,7 @@ class Home extends React.Component {
         return (
             <div className='row'>
                 <div className='col-lg-5 mt-3 mb-3'>
+                    <img className='mb-3' alt='me.png' src='/me.png' height='75' width='75'></img>
                     <p>
                         I live in Dublin and I'm a software developer with an interest in data engineering and cloud computing.
                     </p>
@@ -18,10 +19,10 @@ class Home extends React.Component {
                         I was born in the Philippines, grew up in Ireland and previously studied applied computing at WIT.
                     </p>
                     <p>
-                        I'm working on <a href='https://www.softwareletters.com' target='_blank' rel='noopener noreferrer'>software letters</a>.
+                        I'm working on <a href='https://www.softwareletters.com' target='_blank' rel='noopener noreferrer'>Software Letters</a>.
                     </p>
                     <p>
-                        You can find me at my <a href='mailto:jkrclaro@gmail.com'>email</a>, <a href='https://www.linkedin.com/in/johnclaro' target='_blank' rel='noopener noreferrer'>linkedin</a>, <a href='https://www.twitter.com/johnclaro_' target='_blank' rel='noopener noreferrer'>twitter</a>, <a href='https://getpocket.com/@johnclaro' target='_blank' rel='noopener noreferrer'>pocket</a> or <a href='https://github.com/johnclaro' target='_blank' rel='noopener noreferrer'>github</a>
+                        You can find or reach me at my <a href='mailto:jkrclaro@gmail.com'>email</a>, <a href='https://www.linkedin.com/in/johnclaro' target='_blank' rel='noopener noreferrer'>linkedin</a>, <a href='https://www.twitter.com/johnclaro_' target='_blank' rel='noopener noreferrer'>twitter</a>, <a href='https://getpocket.com/@johnclaro' target='_blank' rel='noopener noreferrer'>pocket</a> or <a href='https://github.com/johnclaro' target='_blank' rel='noopener noreferrer'>github</a>
                     </p>
                 </div>
             </div>
