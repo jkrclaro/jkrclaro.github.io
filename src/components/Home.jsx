79d09f08e1e89504a256a593f7e5ba90a58181ca
John Claro <jkrclaro@gmail.com>
2020-05-29 11:54:38 +0100

    More descriptive

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -19,7 +19,7 @@ class Home extends React.Component {
                         I was born in the Philippines, grew up in Ireland and previously studied applied computing at WIT.
                     </p>
                     <p>
-                        Previously worked at <a href='https://www.distilledsch.ie' target='_blank' rel='noopener noreferrer'>DistilledSCH</a>, now <a href='https://www.sloopback.com' target='_blank' rel='noopener noreferrer'>Sloopback</a>.
+                        Previously worked as data engineer at <a href='https://www.distilledsch.ie' target='_blank' rel='noopener noreferrer'>DistilledSCH</a>, now started <a href='https://www.sloopback.com' target='_blank' rel='noopener noreferrer'>Sloopback</a>.
                     </p>
                     <p>
                         You can find or reach me at my <a href='mailto:jkrclaro@gmail.com'>email</a>, <a href='https://www.linkedin.com/in/johnclaro' target='_blank' rel='noopener noreferrer'>linkedin</a>, <a href='https://www.twitter.com/johnclaro_' target='_blank' rel='noopener noreferrer'>twitter</a>, <a href='https://getpocket.com/@johnclaro' target='_blank' rel='noopener noreferrer'>pocket</a> or <a href='https://github.com/johnclaro' target='_blank' rel='noopener noreferrer'>github</a>
