b765af3d79545d3f8701c743faed7c1be49fdb34
John Claro <jkrclaro@gmail.com>
2020-06-06 23:13:27 +0100

    Update fonts

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,8 +12,7 @@ class Navbar extends React.Component {
         const { pathname } = this.state;
         return (
             <div className='nav mt-3 mb-3'>
-                <h6 style={{paddingRight: 20}}><a className={pathname === '/' ? 'active' : ''} href='/'>About</a></h6>
-                {/* <h6><a className={pathname === '/books' ? 'active' : ''} href='/books'>Books</a></h6> */}
+                <h6 style={{paddingRight: 20}}><a className={pathname === '/' ? 'active' : ''} href='/'>ABOUT</a></h6>
             </div>
         )
     }
