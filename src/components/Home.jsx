b69d3f6c5bbe6c273798db78ed8a0563b921c105
John Claro <jkrclaro@gmail.com>
2020-07-29 20:27:26 +0100

    Simplify

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -14,7 +14,6 @@ class Home extends React.Component {
                     <img className='mb-3' alt='me.png' src='/me.png' height='75' width='75' style={{borderRadius: '50%'}}></img>
                     <p>
                         I was born in the Philippines, grew up in Ireland and previously studied applied computing at WIT.
-                        I work on <a href='https://www.sloopback.com' target='_blank' rel='noopener noreferrer'>Sloopback</a>.
                     </p>
                     <p>
                         You can find or reach me at my <a href='mailto:jkrclaro@gmail.com'>email</a>, <a href='https://www.linkedin.com/in/johnclaro' target='_blank' rel='noopener noreferrer'>linkedin</a>, <a href='https://www.twitter.com/johnclaro_' target='_blank' rel='noopener noreferrer'>twitter</a>, <a href='https://getpocket.com/@johnclaro' target='_blank' rel='noopener noreferrer'>pocket</a> or <a href='https://github.com/johnclaro' target='_blank' rel='noopener noreferrer'>github</a>.
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,7 +12,7 @@ class Navbar extends React.Component {
         const { pathname } = this.state;
         return (
             <div className='nav mt-3 mb-3'>
-                <h6 style={{paddingRight: 20}}><a className={pathname === '/' ? 'active' : ''} href='/'>ABOUT</a></h6>
+                <h6 style={{paddingRight: 20}}><a className={pathname === '/' ? 'active' : ''} href='/'>About</a></h6>
                 {/* <h6 style={{paddingRight: 20}}><a className={pathname === '/essays' ? 'active' : ''} href='/essays'>ESSAYS</a></h6> */}
             </div>
         )
