e6162a2b122873318410deda3545aa18b24e4991
John Claro <jkrclaro@gmail.com>
2020-07-31 11:00:56 +0100

    Use ===

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,7 +14,7 @@ class Navbar extends React.Component {
             <div style={{paddingBottom: 10}}>
                 <li style={{display: 'inline'}}>
                     <a href='/' className={pathname === '/' ? 'active' : ''} style={{paddingRight: 15}}>About</a>
-                    <a href='/notes' className={pathname == '/notes' || pathname.includes('/notes/') ? 'active' : ''}>Notes</a>
+                    <a href='/notes' className={pathname === '/notes' || pathname.includes('/notes/') ? 'active' : ''}>Notes</a>
                 </li>
             </div>
         )
