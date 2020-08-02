bd0c3507b208f25d67dcc8ab277d241a752257d5
John Claro <jkrclaro@gmail.com>
2020-08-02 12:55:15 +0100

    Currently

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -20,7 +20,7 @@ class About extends React.Component {
                     I was born in the Philippines, grew up in Ireland and previously studied applied computing at WIT.
                 </p>
                 <p>
-                    I work as a freelance developer and have a side project called <a href='https://www.sloopback.com' target='_blank' rel='noopener noreferrer'>Sloopback</a>.
+                    I'm currently a freelance developer and have a side project called <a href='https://www.sloopback.com' target='_blank' rel='noopener noreferrer'>Sloopback</a>.
                 </p>
                 <p>
                     Feel free to contact me at my <a href='mailto:jkrclaro@gmail.com'>email</a>, <a href='https://www.linkedin.com/in/johnclaro' target='_blank' rel='noopener noreferrer'>linkedin</a>, <a href='https://www.twitter.com/johnclaro_' target='_blank' rel='noopener noreferrer'>twitter</a> or <a href='https://github.com/johnclaro' target='_blank' rel='noopener noreferrer'>github</a>.
