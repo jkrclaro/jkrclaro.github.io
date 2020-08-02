cf9ca188aecf11b74138fbc454322f3219ea54c3
John Claro <jkrclaro@gmail.com>
2020-08-02 16:31:57 +0100

    Update About

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,11 +14,11 @@ class About extends React.Component {
     render() {
         const { latest } = this.state;
         return (
-            <div style={{maxWidth: 500}}>
+            <div style={{maxWidth: 525}}>
                 <img alt='me.png' src='/me.png' height='100' width='100' style={{borderRadius: 50}}></img>
                 <p>
                     I was born in the Philippines, grew up in Ireland and previously studied applied computing at WIT.
-                    I work on <a href='https://www.sloopback.com' target='_blank' rel='noopener noreferrer'>Sloopback</a>. 
+                    Working on <a href='https://www.sloopback.com' target='_blank' rel='noopener noreferrer'>Sloopback</a>. 
                 </p>
                 <p>
                     Contact me at my <a href='mailto:jkrclaro@gmail.com'>email</a>, <a href='https://www.linkedin.com/in/johnclaro' target='_blank' rel='noopener noreferrer'>linkedin</a>, <a href='https://www.twitter.com/johnclaro_' target='_blank' rel='noopener noreferrer'>twitter</a>, <a href='https://dribbble.com/johnclaro' target='_blank' rel='noopener noreferrer'>dribbble</a> or <a href='https://github.com/johnclaro' target='_blank' rel='noopener noreferrer'>github</a>.
