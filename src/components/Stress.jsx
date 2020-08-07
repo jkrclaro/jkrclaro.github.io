f376c4111ff76c50c22457f0bdf3b4a789d52852
John Claro <jkrclaro@gmail.com>
2020-08-08 00:16:31 +0100

    Text

--- src/components/Stress.jsx
+++ src/components/Stress.jsx
@@ -14,6 +14,24 @@ class Stress extends React.Component {
                     <source src='/stress.mp4' type='video/mp4'></source>
                     Your browser does not support the video tag.
                 </video>
+                <p>
+                    <b>Stress primarily comes from not taking action</b> over something you can have some control over.
+                </p>
+                <p>
+                    If I find that some particular thing is causing me to have stress, that's a warning flag.
+                </p>
+                <p>
+                    It means there's something that I haven't completely identified in my conscious mind that is bothering me and I haven't yet taken any action on it.
+                </p>
+                <p>
+                    I find that <b>as soon as I identify it and make the first action to address that situation</b>, even if it's not solved, the mere fact that we're addresssing it dramatically reduces stress.
+                </p>
+                <p>
+                    So <b>stress comes from ignoring things that you shouldn't be ignoring</b>.
+                </p>
+                <p>
+                    <b>Stress doesn't come from hard work</b>, you can be working extremely hard and loving it, likewise, you can be out of work and incredibly stressed.
+                </p>
             </div>
         )
     }
