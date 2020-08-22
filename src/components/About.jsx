840e7af5099f3c92918721501a2c3635dd0774c0
John Claro <jkrclaro@gmail.com>
2020-08-22 23:51:48 +0100

    Update links

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -18,10 +18,10 @@ class About extends React.Component {
                 <img alt='me.png' src='/me.png' height='100' width='100' style={{borderRadius: 50}}></img>
                 <p>
                     I was born in the Philippines, grew up in Ireland and previously studied applied computing at WIT.
-                    I'm working on <a href='https://www.sloopback.com' target='_blank' rel='noopener noreferrer'>Sloopback</a>. 
+                    I work on <a href='https://www.trysloop.com' target='_blank' rel='noopener noreferrer'>Sloop</a>. 
                 </p>
                 <p>
-                    Contact me at my <a href='mailto:jkrclaro@gmail.com'>email</a>, <a href='https://www.linkedin.com/in/johnclaro' target='_blank' rel='noopener noreferrer'>linkedin</a>, <a href='https://www.twitter.com/johnclaroo' target='_blank' rel='noopener noreferrer'>twitter</a> or <a href='https://github.com/johnclaro' target='_blank' rel='noopener noreferrer'>github</a>.
+                    Contact me at my <a href='mailto:jkrclaro@gmail.com'>email</a>, <a href='https://www.linkedin.com/in/johnclaro' target='_blank' rel='noopener noreferrer'>linkedin</a>, <a href='https://www.twitter.com/johnclaro_' target='_blank' rel='noopener noreferrer'>twitter</a> or <a href='https://github.com/johnclaro' target='_blank' rel='noopener noreferrer'>github</a>.
                 </p>
                 <p>
                     My latest blog is <a href={latest.url}>{latest.title}</a>.
