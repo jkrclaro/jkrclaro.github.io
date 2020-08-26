97d6f57bdb46094fd0b962e130bb56a556e2d04e
John Claro <jkrclaro@gmail.com>
2020-08-26 16:07:22 +0100

    Update email

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -21,7 +21,7 @@ class About extends React.Component {
                     I worked as data engineer for <a href='https://www.distilledsch.ie' target='_blank' rel='noopener noreferrer'>DistilledSCH</a> after graduation and left to work on <a href='https://www.trysloop.com' target='_blank' rel='noopener noreferrer'>Sloop</a>. 
                 </p>
                 <p>
-                    Contact me at my <a href='mailto:jkrclaro@gmail.com'>email</a>, <a href='https://www.linkedin.com/in/johnclaro' target='_blank' rel='noopener noreferrer'>linkedin</a>, <a href='https://www.twitter.com/johnclaro_' target='_blank' rel='noopener noreferrer'>twitter</a> or <a href='https://github.com/johnclaro' target='_blank' rel='noopener noreferrer'>github</a>.
+                    Contact me at my <a href='mailto:john@trysloop.com'>email</a>, <a href='https://www.linkedin.com/in/johnclaro' target='_blank' rel='noopener noreferrer'>linkedin</a>, <a href='https://www.twitter.com/johnclaro_' target='_blank' rel='noopener noreferrer'>twitter</a> or <a href='https://github.com/johnclaro' target='_blank' rel='noopener noreferrer'>github</a>.
                 </p>
                 {/* <p>
                     My latest blog is <a href={latest.url}>{latest.title}</a>.
