068c04d637ad311168bbfd692c24dd7eb7db9be5
John Claro <jkrclaro@gmail.com>
2020-09-19 00:45:34 +0100

    Change to Censusfy

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -18,7 +18,7 @@ class About extends React.Component {
                 <img alt='me.png' src='/me.png' height='100' width='100' style={{borderRadius: 50}}></img>
                 <p>
                     I was born in the Philippines, grew up in Ireland and studied applied computing at <a href='https://www.wit.ie' target='_blank' rel='noopener noreferrer'>WIT</a>.
-                    I worked as data engineer for <a href='https://www.distilledsch.ie' target='_blank' rel='noopener noreferrer'>DistilledSCH</a> after graduation and left to work on <a href='https://www.trysloop.com' target='_blank' rel='noopener noreferrer'>Sloop</a>. 
+                    I worked as data engineer for <a href='https://www.distilledsch.ie' target='_blank' rel='noopener noreferrer'>DistilledSCH</a> after graduation and left to work on <a href='https://www.censusfy.com' target='_blank' rel='noopener noreferrer'>Censusfy</a>. 
                 </p>
                 <p>
                     Contact me at my <a href='mailto:john@trysloop.com'>email</a>, <a href='https://www.linkedin.com/in/johnclaro' target='_blank' rel='noopener noreferrer'>linkedin</a>, <a href='https://www.twitter.com/johnclaro_' target='_blank' rel='noopener noreferrer'>twitter</a> or <a href='https://github.com/johnclaro' target='_blank' rel='noopener noreferrer'>github</a>.
