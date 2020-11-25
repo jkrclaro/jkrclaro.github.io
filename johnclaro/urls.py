72eedcce1311733f14c77cd71fb2855dfc4aae33
John Claro <jkrclaro@gmail.com>
2020-11-25 23:34:50 +0000

    Bookshelf route

--- johnclaro/urls.py
+++ johnclaro/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include
 from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name='landingpage.html'))
+    path('', TemplateView.as_view(template_name='landingpage.html')),
+    path('bookshelf/', include('johnclaro.bookshelf.urls'), name='bookshelf')
 ]
