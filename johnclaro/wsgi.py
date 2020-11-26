231ab445ffa6537883be8f39359e6de7d8d50379
John Claro <jkrclaro@gmail.com>
2020-11-26 21:59:50 +0000

    Add PYTHON_EGG_CACHE

--- johnclaro/wsgi.py
+++ johnclaro/wsgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
+os.environ.setdefault('PYTHON_EGG_CACHE', '/opt/bitnami/projects/johnclaro-blog/egg_cache')
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'johnclaro.settings')
 
 application = get_wsgi_application()
