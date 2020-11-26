c88a166c540ede33819af5b8cbe6031c8e13e204
John Claro <jkrclaro@gmail.com>
2020-11-26 23:52:06 +0000

    Fixing staticfiles issue

--- johnclaro/settings.py
+++ johnclaro/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '7-q@t8j&&irukf@&qmmst!k5g&j!jh^$ar&u3pj_g4c1myg8f#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '*'
