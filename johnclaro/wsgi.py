b4f4216c8a6a4115d767669f2abc5f5ec85bea52
John Claro <jkrclaro@gmail.com>
2021-03-02 20:05:47 +0000

    Update for Approach A on bitnami

--- johnclaro/wsgi.py
+++ johnclaro/wsgi.py
@@ -1,19 +1,7 @@
-"""
-WSGI config for johnclaro project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
-"""
-
 import os
-import sys
 
 from django.core.wsgi import get_wsgi_application
 
-sys.path.append('/home/bitnami/website')
-os.environ.setdefault('PYTHON_EGG_CACHE', '/home/bitnami/website/egg_cache')
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'johnclaro.settings')
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'johnclaro.settings')
 application = get_wsgi_application()
