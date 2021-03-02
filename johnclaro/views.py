8ce1983a7954785af90f70a1de3d54fc39497161
John Claro <jkrclaro@gmail.com>
2021-03-02 17:37:17 +0000

    Add csrf_exempt on upsert_covid

--- johnclaro/views.py
+++ johnclaro/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 
 def show_covid(request):
     return render(request, 'covid.html')
 
 
+@csrf_exempt
 def upsert_covid(request):
     if request.method == 'POST':
-        print('Got this data', request)
+        print(request.POST)
     return JsonResponse({'status': 200})
