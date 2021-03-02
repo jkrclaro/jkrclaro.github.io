238d04013b63b9135e716cddef2269a14bb883c7
John Claro <jkrclaro@gmail.com>
2021-03-02 20:47:22 +0000

    Update conf

--- launch.sh
+++ launch.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-cp /home/bitnami/website/conf/bitnami.conf /opt/bitnami/apache2/conf/bitnami/bitnami.conf
+cp /home/bitnami/website/conf/sample-vhost.conf /opt/bitnami/apache2/conf/vhosts/sample-vhost.conf
+cp /home/bitnami/website/conf/sample-https-vhost.conf /opt/bitnami/apache2/conf/vhosts/sample-https-vhost.conf
 /opt/bitnami/ctlscript.sh restart apache
