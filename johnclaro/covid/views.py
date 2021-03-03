efd077359a4725ededf0d91b3c06ce14555f8434
John Claro <jkrclaro@gmail.com>
2021-03-03 22:31:14 +0000

    Remove pandas

--- johnclaro/covid/views.py
+++ johnclaro/covid/views.py
@@ -1,13 +1,7 @@
-import pickle
-import base64
-import logging
-
 from django.shortcuts import render
 from django.http import JsonResponse
 
 from .models import Covid
-
-logger = logging.getLogger(__name__)
 
 
 def show_covid(request):
@@ -16,19 +10,14 @@ def show_covid(request):
 
 
 def upsert_covid(request):
-    logger.info(request)
     if request.method == 'POST':
-        df_b64 = request.POST.get('df_b64')
-        df = pickle.loads(base64.b64decode(df_b64.encode()))
-
-        for index, row in df.iterrows():
-            Covid.objects.upsert_covid(
-                date=row.date,
-                country=row.country,
-                cases=row.cases,
-                deaths=row.deaths,
-                recoveries=row.recoveries
-            )
+        Covid.objects.upsert_covid(
+            date=request.POST.get('date'),
+            country=request.POST.get('country'),
+            cases=request.POST.get('cases'),
+            deaths=request.POST.get('deaths'),
+            recoveries=request.POST.get('recoveries')
+        )
         return JsonResponse({'status': 'Successfully upserted covid data'})
     else:
         return JsonResponse({'status': 'Got GET request'})
