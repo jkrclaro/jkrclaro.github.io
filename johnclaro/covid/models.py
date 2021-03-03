33e35d52642191e89e5aa320d6f29fb58453fc99
John Claro <jkrclaro@gmail.com>
2021-03-03 15:03:25 +0000

    Create Covid model

--- johnclaro/covid/models.py
+++ johnclaro/covid/models.py
@@ -1,16 +1,34 @@
 from django.db import models
+from django.db.utils import IntegrityError
+
+
+class CovidManager(models.Manager):
+    def upsert_covid(self, date, country, cases, deaths, recovered):
+        try:
+            covid = self.create(
+                date=date,
+                country=country,
+                cases=cases,
+                deaths=deaths,
+                recovered=recovered
+            )
+            return covid
+        except IntegrityError:
+            pass
 
 
 class Covid(models.Model):
     date = models.DateTimeField()
     country = models.CharField(max_length=255)
-    state = models.CharField(max_length=255)
     cases = models.IntegerField()
     deaths = models.IntegerField()
     recovered = models.FloatField()
+    objects = CovidManager()
 
     def __str__(self):
-        return f'{self.date}-{self.country}-{self.state}-{self.cases}-{self.deaths}-{self.recovered}'
+        return f'{self.date}-{self.country}-{self.cases}-{self.deaths}-' \
+                f'{self.recovered}'
 
     class Meta:
         db_table = 'covids'
+        unique_together = ('date', 'country', 'cases', 'deaths', 'recovered',)
--- johnclaro/covid/views.py
+++ johnclaro/covid/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 
+from .models import Covid
+
 
 def show_covid(request):
     return render(request, 'covid.html')
@@ -11,8 +13,16 @@ def show_covid(request):
 def upsert_covid(request):
     if request.method == 'POST':
         date = request.POST.get('date')
+        country = request.POST.get('country')
         cases = request.POST.get('cases')
         deaths = request.POST.get('deaths')
         recovered = request.POST.get('recovered')
-        
+        Covid.objects.upsert_covid(
+            date=date,
+            country=country,
+            cases=cases,
+            deaths=deaths,
+            recovered=recovered
+        )
+
     return JsonResponse({'status': 200})
