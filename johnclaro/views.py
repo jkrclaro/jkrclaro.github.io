1422be1cfca436cf9e70271014d72286e16fd68e
John Claro <jkrclaro@gmail.com>
2021-03-03 14:28:44 +0000

    Update bio

--- johnclaro/views.py
+++ johnclaro/views.py
@@ -10,5 +10,9 @@ def show_covid(request):
 @csrf_exempt
 def upsert_covid(request):
     if request.method == 'POST':
-        print(request.POST)
+        date = request.POST.get('date')
+        cases = request.POST.get('cases')
+        deaths = request.POST.get('deaths')
+        recovered = request.POST.get('recovered')
+        
     return JsonResponse({'status': 200})
