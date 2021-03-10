c5e9ff64bfbeeac1f7f935c5658c1cbe0d6eb80c
John Claro <jkrclaro@gmail.com>
2021-03-10 19:04:06 +0000

    Remove legend on bar chart

--- static/js/covid/counties.js
+++ static/js/covid/counties.js
@@ -1,7 +1,7 @@
 Highcharts.chart('counties-chart', {
     chart: {
         type: 'bar',
-        height: 500,
+        height: 768,
     },
     title: {
         text: 'Cases by county'
@@ -28,18 +28,6 @@ Highcharts.chart('counties-chart', {
                 enabled: true
             }
         }
-    },
-    legend: {
-        layout: 'vertical',
-        align: 'right',
-        verticalAlign: 'top',
-        x: -40,
-        y: 80,
-        floating: true,
-        borderWidth: 1,
-        backgroundColor:
-            Highcharts.defaultOptions.legend.backgroundColor || '#FFFFFF',
-        shadow: true
     },
     credits: {
         enabled: false
