25eb9d6fb8f478661ea337c7b7f9ad095fa402a2
John Claro <jkrclaro@gmail.com>
2021-03-11 22:45:04 +0000

    Prepend covid to url

---
+++ johnclaro/covid/urls.py
@@ -0,0 +1,23 @@
+from django.urls import path
+
+from .views import (
+    show_cases,
+    show_swabs,
+    show_vaccines,
+    johnhopkins_cases_upsert,
+    hse_cases_upsert,
+    hse_swabs_upsert,
+    hse_counties_upsert,
+)
+
+app_name = 'covid'
+
+urlpatterns = [
+    path('', show_cases, name='cases'),
+    path('covid/swabs', show_swabs, name='swabs'),
+    path('covid/vaccines', show_vaccines, name='vaccines'),
+    path('johnhopkins/cases/upsert', johnhopkins_cases_upsert, name='johnhopkins_cases_upsert'),
+    path('hse/cases/upsert', hse_cases_upsert, name='hse_cases_upsert'),
+    path('hse/swabs/upsert', hse_swabs_upsert, name='hse_swabs_upsert'),
+    path('hse/counties/upsert', hse_counties_upsert, name='hse_counties_upsert'),
+]
--- johnclaro/urls.py
+++ johnclaro/urls.py
@@ -5,29 +5,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls import include
 
-from .covid.views import (
-    show_cases,
-    show_swabs,
-    show_vaccines,
-    johnhopkins_cases_upsert,
-    hse_cases_upsert,
-    hse_swabs_upsert,
-    hse_counties_upsert,
-)
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='bio.html'), name='bio'),
-    path('covid', show_cases, name='cases'),
-    path('covid/swabs', show_swabs, name='swabs'),
-    path('covid/vaccines', show_vaccines, name='vaccines'),
-    path('johnhopkins/cases/upsert', johnhopkins_cases_upsert, name='johnhopkins_cases_upsert'),
-    path('hse/cases/upsert', hse_cases_upsert, name='hse_cases_upsert'),
-    path('hse/swabs/upsert', hse_swabs_upsert, name='hse_swabs_upsert'),
-    path('hse/counties/upsert', hse_counties_upsert, name='hse_counties_upsert'),
-
+    path('covid/', include('johnclaro.covid.urls'), name='covid'),
     path('podplayer/', include('johnclaro.podplayer.urls'), name='podplayer'),
-
     path('accounts/', include('johnclaro.accounts.urls'), name='accounts'),
 ]
 
