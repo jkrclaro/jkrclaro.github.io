07e96ab2ee2db2e6866819864b4cafe863b27bc7
John Claro <jkrclaro@gmail.com>
2021-03-12 13:37:07 +0000

    Update settings for SIMPLE_JWT and CORS_ALLOWED_ORIGINS

--- johnclaro/environment/prod.py
+++ johnclaro/environment/prod.py
@@ -6,6 +6,6 @@ from ..settings import BASE_DIR
 DEBUG = False
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-CORS_ALLOW_ALL_ORIGINS = True
-CSRF_COOKIE_SECURE = False
-SESSION_COOKIE_SECURE = False
+CORS_ALLOWED_ORIGINS = [
+    'https://podplayer.vercel.app/'
+]
