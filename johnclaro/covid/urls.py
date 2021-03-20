17de5b3c704278dfef5c022a2880bf2529bed7a6
John Claro <jkrclaro@gmail.com>
2021-03-20 18:37:00 +0000

    Merge all HSE datasets into one API

--- johnclaro/covid/urls.py
+++ johnclaro/covid/urls.py
@@ -4,10 +4,8 @@ from .views import (
     show_cases,
     show_swabs,
     show_vaccines,
-    johnhopkins_cases_upsert,
-    hse_cases_upsert,
-    hse_swabs_upsert,
-    hse_counties_upsert,
+    johnhopkins_upsert,
+    hse_upsert,
 )
 
 app_name = 'covid'
@@ -16,8 +14,6 @@ urlpatterns = [
     path('', show_cases, name='cases'),
     path('swabs', show_swabs, name='swabs'),
     path('vaccines', show_vaccines, name='vaccines'),
-    path('johnhopkins/cases/upsert', johnhopkins_cases_upsert, name='johnhopkins_cases_upsert'),
-    path('hse/cases/upsert', hse_cases_upsert, name='hse_cases_upsert'),
-    path('hse/swabs/upsert', hse_swabs_upsert, name='hse_swabs_upsert'),
-    path('hse/counties/upsert', hse_counties_upsert, name='hse_counties_upsert'),
+    path('johnhopkins/upsert', johnhopkins_upsert, name='johnhopkins_upsert'),
+    path('hse/upsert', hse_upsert, name='hse_upsert'),
 ]
