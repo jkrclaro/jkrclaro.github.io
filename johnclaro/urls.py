88aff661f4704d967dacd04f759d9e4fd4632b97
John Claro <jkrclaro@gmail.com>
2021-03-21 18:25:19 +0000

    Add ironman converter

--- johnclaro/urls.py
+++ johnclaro/urls.py
@@ -9,6 +9,7 @@ from django.conf.urls import include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='bio.html'), name='bio'),
+    path('ironman', TemplateView.as_view(template_name='ironman.html'), name='ironman'),
     path('covid/', include('johnclaro.covid.urls'), name='covid'),
     path('podplayer/', include('johnclaro.podplayer.urls'), name='podplayer'),
     path('accounts/', include('johnclaro.accounts.urls'), name='accounts'),
